ea31dac9762b5ea1307d71516ea5a0863fa76e6d
hyunbae.jeon <mazingazz@gmail.com>
2023-07-27 16:21:03 +0900

    baseurl

--- src/app/cafes/page.tsx
+++ src/app/cafes/page.tsx
@@ -1,7 +1,10 @@
+import Button from 'components/input/button'
+
 export default function Loin() {
   return (
     <main>
       <div>Cafe list</div>
+      <Button/>
     </main>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import '@/asset/styles/globals.css'
+import 'asset/styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
