fbad8f180c574816673b2701355922a652637de4
hyunbae.jeon <mazingazz@gmail.com>
2023-08-04 15:20:22 +0900

    search box

--- src/components/molecules/searchBox.tsx
+++ src/components/molecules/searchBox.tsx
@@ -1,23 +1,98 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import FormControl from '@mui/material/FormControl';
+import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import FilledInput from '@mui/material/FilledInput';
 import OutlinedInput from '@mui/material/OutlinedInput';
+import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
+import Button from '@mui/material/Button';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
 import { styled } from '@mui/system';
 
 const searchBox = ({ className, children }: any) => (
   <div className={className}>
     <FormControl fullWidth sx={{ m: 1 }}>
-      <InputLabel htmlFor='outlined-adornment-amount'>Search</InputLabel>
-      <OutlinedInput id='outlined-adornment-amount' label='Amount' />
+      <TextField id='outlined-search' label='Search field' type='search' />
     </FormControl>
   </div>
 );
 
-const StyledSearchBox = styled(searchBox)`
-  margin-right: 14px;
+const SearchWrapper = styled('div')`
+  display: inline-flex;
+  width: 100%;
+  margin: 0 0 2rem 0 ;
+  padding: 0 8px;
+  > div:nth-of-type(1) {
+    width: 60%;
+    text-align: right;
+    .MuiFormControl-root {
+      width: 60%;
+    }
+  }
+  > div:nth-of-type(2) {
+    display: flex;
+    width: 10%;
+    align-items: center;
+    justify-content: center;
+    button {
+      margin-top: -2px;
+      height: 93%;
+      background-color: #1976d2;
+    }
+  }
+  > div:nth-of-type(3) {
+    display: flex;
+    width: 30%;
+    align-items: center;
+    > div {
+      min-width:7rem
+    }
 `;
 
-export default function SearchBox() {
-  return <StyledSearchBox />;
+export default function SearchBox(props: any) {
+  const { lastPage, setCafePageNo } = props;
+  const noOfPages = useMemo(() => lastPage, [lastPage]);
+  const pageList = useMemo(() => (noOfPages ? new Array(noOfPages).fill(0).map((_, i) => i + 1) : [1]), [noOfPages]);
+  const [page, setPage] = useState('1');
+
+  const handleChange = useCallback(
+    (event: SelectChangeEvent) => {
+      const pageNo = event.target.value as string;
+
+      setPage(pageNo);
+      setCafePageNo(pageNo);
+    },
+    [setCafePageNo],
+  );
+
+  return (
+    <SearchWrapper>
+      <div>
+        <TextField id='outlined-search' label='Search field' type='search' />
+      </div>
+      <div>
+        <Button variant='contained' size='large'>
+          Search
+        </Button>
+      </div>
+      <div>
+        <Select
+          labelId='demo-simple-select-label'
+          id='demo-simple-select'
+          value={page}
+          label='Age'
+          onChange={handleChange}
+        >
+          {pageList?.map((item, index) => (
+            <MenuItem key={index} value={item}>
+              {item}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
+    </SearchWrapper>
+  );
 }
--- src/components/organisms/cafe/list.tsx
+++ src/components/organisms/cafe/list.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useCafeQuery } from 'store/cafe';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
@@ -8,14 +8,25 @@ import SearchBox from 'components/molecules/searchBox';
 import Card from 'components/molecules/card';
 
 export default function List() {
-  const { isLoading, isError, data } = useCafeQuery({ page: 1 });
+  const [page, setPage] = useState(1);
+  const { isLoading, isError, data } = useCafeQuery({ page });
 
+  const [lastPage, setLastPage] = useState(0);
   const currentPage = useMemo(() => data?.current_page, [data]);
   const cafeDatas = useMemo(() => data?.data, [data]);
+  const setCafePageNo = useCallback((pageNo: number) => {
+    setPage(pageNo);
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      if (data.last_page !== lastPage) setLastPage(data.last_page);
+    }
+  }, [lastPage, data]);
 
   return (
     <>
-      <SearchBox />
+      <SearchBox lastPage={lastPage} setCafePageNo={setCafePageNo} />
       {isLoading && <div>isLoading...</div>}
       {cafeDatas && (
         <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1200: 4 }}>
