032fa4ee631428b60be50fc09f7aed41518acb1c
hyunbae.jeon <mazingazz@gmail.com>
2023-08-18 17:48:34 +0900

    random topic

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -7,9 +7,9 @@ import Words from 'components/molecules/words';
 import List from 'components/organisms/news/list';
 
 export default async function News(props: any) {
-  const params = { q: 'apple', from: '2023-08-16', to: '2023-08-16', sortBy: 'popularity', page: 1, pageSize: 10 };
-  const initialData = await fetchNews(params);
   const topics = generate(20);
+  const params = { q: topics[0], from: '2023-08-16', to: '2023-08-16', sortBy: 'popularity', page: 1, pageSize: 10 };
+  const initialData = await fetchNews(params);
 
   return (
     <main>
