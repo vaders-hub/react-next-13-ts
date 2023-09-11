30405fb382cdd6f0fad97d9ec9d53c373513b0de
hyunbae.jeon <mazingazz@gmail.com>
2023-09-11 17:52:07 +0900

    date

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import dynamic from 'next/dynamic';
+
+import dayjs from 'dayjs';
+
+import PageLoader from 'components/atoms/pageLoader';
+import Title from 'components/atoms/title';
+import DateConfig from 'components/molecules/dateConfig';
+import Words from 'components/molecules/words';
+
+import { fetchNews } from 'store/news';
 
 interface PageProps {
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default async function News({ params }: PageProps) {
-  return <main></main>;
+const List = dynamic(() => import('components/organisms/news/list'), {
+  loading: () => <PageLoader />,
+});
+
+export default async function News({ searchParams }: PageProps) {
+  const today = dayjs(new Date()).format('YYYY-MM-DD');
+  const yesterday = dayjs(new Date()).subtract(1, 'day').format('YYYY-MM-DD');
+  const topic = Object.entries(searchParams).length ? searchParams.topic : 'soccer';
+  const param = {
+    q: topic,
+    from: yesterday,
+    to: today,
+    sortBy: 'popularity',
+    page: 1,
+    pageSize: 10,
+  };
+  const initialData: any = [];
+
+  try {
+    const result = await fetchNews(param);
+
+    if (result.articles.length) Object.assign(initialData, result.articles);
+  } catch (e: any) {
+    console.log('e', e.response.status);
+  }
+
+  return (
+    <>
+      <Suspense>
+        <Title title={'News Feed'} />
+        <div>
+          <Words />
+          <DateConfig today={today} yesterday={yesterday} />
+          {initialData.length > 0 && <List initialData={initialData} />}
+        </div>
+      </Suspense>
+    </>
+  );
 }
--- src/components/molecules/dateConfig.tsx
+++ src/components/molecules/dateConfig.tsx
@@ -8,19 +8,24 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { DatePicker } from '@mui/x-date-pickers';
 
+interface DateConfigs {
+  today?: string;
+  yesterday?: string;
+}
+
 const StyledConfig = styled('div')(({ theme }) => ({
   margin: '1rem 0',
   textAlign: 'right',
   '& > div': { marginLeft: '0.5rem' },
 }));
 
-export default function DateConfig({ today }: { today?: string }) {
+export default function DateConfig({ today, yesterday }: DateConfigs) {
   return (
     <StyledConfig>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker
           label='Start'
-          defaultValue={dayjs(today)}
+          defaultValue={dayjs(yesterday)}
           slotProps={{ field: { shouldRespectLeadingZeros: true } }}
         />
         <DatePicker
