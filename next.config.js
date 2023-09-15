1e075cab9658d078304749718a775f6ff576b20a
hyunbae.jeon <mazingazz@gmail.com>
2023-09-15 18:06:09 +0900

    env

--- next.config.js
+++ next.config.js
@@ -31,6 +31,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/api/:path*',
+      },
+    ];
+  },
   devIndicators: {
     buildActivityPosition: 'bottom-right',
   },
